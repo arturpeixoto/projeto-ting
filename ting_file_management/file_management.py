67a704843935d10b86d2fb99ee9c811195761761
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2023-11-01 11:12:14 -0300

    feat filemanagement

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, 'r') as file:
+            if path_file.endswith(".txt"):
+                lines = file.read()
+                return lines.split('\n')
+            else:
+                print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
